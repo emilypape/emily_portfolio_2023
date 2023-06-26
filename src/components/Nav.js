a004927107ab9344eabfe6db3369f704f58ec9b4
emily <eap6787@gmail.com>
2023-06-26 17:14:09 -0600

    create initial nav

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+export default function Nav() {
+  return (
+    <div className='xl:ml-60 md:ml-16 lg:ml-64'>
+      <div className='hidden lg:flex md:flex xl:flex text-gray-500 font-md font-semibold mt-14 mb-14'>
+        <div className='hover:text-black xl:mr-10  lg:mr-10 md:mr-6'>Bio</div>
+        <div className='hover:text-black xl:mr-10  lg:mr-10 md:mr-6'>Featured</div>
+        <div className='hover:text-black xl:mr-10  lg:mr-10 md:mr-6'>Portfolio</div>
+        <div className='hover:text-black xl:mr-10 lg:mr-10 md:mr-6'>Resume</div>
+        <div className='hover:text-black xl:mr-10 lg:mr-10 md:mr-6'>Contact</div>
+      </div>
+    </div>
+  );
+}
