cb1fd52ec06e878d328e835b5c81402a701c8849
emily <eap6787@gmail.com>
2023-07-14 16:21:02 -0600

    add required asterick for inputs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -47,7 +47,7 @@ export default function Contact() {
                   type='email'
                   id='email'
                   class='shadow-sm bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 '
-                  placeholder='email'
+                  placeholder='email*'
                   required
                 />
               </div>
@@ -56,7 +56,7 @@ export default function Contact() {
                   type='text'
                   id='subject'
                   class='block p-3 w-full text-sm text-gray-900 bg-white-50 rounded-lg border border-gray-400 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-white-700 dark:border-gray-300 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-                  placeholder='subject'
+                  placeholder='subject*'
                   required
                 />
               </div>
