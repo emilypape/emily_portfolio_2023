3bdbf947cc73452c7c8856c0ea819fc34fdcb083
emily <eap6787@gmail.com>
2023-07-18 15:41:26 -0600

    fix margins on portfolio component for mobile ui

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,11 +6,11 @@ import { Icon } from '@iconify/react';
 export default function Portfolio() {
   return (
     <div>
-      <div className='text-start text-2xl text-black font-bold xl:ml-52 lg:ml-52 md:ml-16 xl:mr-52 lg:mr-52 md:mr-16 mt-14'>
+      <div className='text-start ml-16 text-2xl text-black font-bold xl:ml-52 lg:ml-52 md:ml-16 xl:mr-52 lg:mr-52 md:mr-16 mt-14'>
         Portfolio
       </div>
       <div className='flex '>
-        <div className='relative flex mt-[18em] lg:mt-44 md:mt-44 xl:mt-44 ml-[25em]'>
+        <div className='relative flex mt-[18em] lg:mt-44 md:mt-44 xl:mt-44 lg:ml-[25em] md:ml-[25em] xl:ml-[25em] ml-[20em]'>
           <div classNameName='w-96 mx-auto'>
             {/* <!-- first --> */}
             <div className=''>
@@ -193,45 +193,6 @@ export default function Portfolio() {
               </div>
             </div>
             {/* <!-- three --> */}
-            <div className=''>
-              <input className='sr-only peer' type='radio' name='carousel' id='carousel-3' />
-              {/* <!-- content #3 --> */}
-              <div className='w-96 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-lg transition-all duration-300 opacity-0 peer-checked:opacity-100 peer-checked:z-10 z-0'>
-                {/* <!-- controls --> */}
-                <div className='absolute top-1/2 w-full flex justify-between z-20'>
-                  <label
-                    for='carousel-2'
-                    className='inline-block text-yellow-600 cursor-pointer -translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='h-10 w-10'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'>
-                      <path
-                        fill-rule='evenodd'
-                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z'
-                        clip-rule='evenodd'
-                      />
-                    </svg>
-                  </label>
-                  <label
-                    for='carousel-1'
-                    className='inline-block text-yellow-600 cursor-pointer translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='h-10 w-10'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'>
-                      <path
-                        fill-rule='evenodd'
-                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z'
-                        clip-rule='evenodd'
-                      />
-                    </svg>
-                  </label>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
         <div className='  max-w-[35em] text-md text-start xl:ml-52 lg:ml-72 md:ml-16 mr-[15em] text-gray-500'>
