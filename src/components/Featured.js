785fc490246f1999ed341fb0c0faca0ee542b062
emily <eap6787@gmail.com>
2023-07-18 15:35:38 -0600

    fix mobile ui for featured skills section

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -4,7 +4,7 @@ export default function Featured() {
   return (
     <div>
       <div className='xl:ml-52 lg:ml-52 md:ml-16 xl:mr-52 lg:mr-52 md:mr-16 mt-10'>
-        <div className='text-start text-2xl text-black font-bold'>Featured Skills</div>
+        <div className='text-start ml-16 lg:ml-0 md:ml-0 xl:ml-0 text-2xl text-black font-bold'>Featured Skills</div>
         <div className='hidden lg:flex md:flex  text-start mt-5'>
           <div className=' min-w-[18em] min-h-[16em] mr-8   w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
             <div className='min-h-[16em] bg-white rounded-md p-4'>
@@ -55,8 +55,8 @@ export default function Featured() {
             </div>
           </div>
         </div>
-        <div className='flex-wrap lg:hidden md:hidden xl:hidden  text-start mt-5'>
-          <div className=' min-w-[18em] min-h-[16em] mr-8   w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
+        <div className='flex-wrap ml-16 lg:hidden md:hidden xl:hidden  text-start mt-5'>
+          <div className=' min-w-[32em] min-h-[16em] mb-5 w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
             <div className='min-h-[16em] bg-white rounded-md p-4'>
               <div className='text-gray-500 font-semibold text-lg  mb-3'>Frontend</div>
               <div className='text-gray-500 mb-8'>
@@ -72,7 +72,7 @@ export default function Featured() {
               </div>
             </div>
           </div>
-          <div className='min-w-[18em] min-h-[16em] mr-8  w-full h-full rounded-md bg-gradient-to-r from-fuchsia-600 via-violet-500 to-indigo-600 p-1'>
+          <div className='min-w-[32em] mb-5 min-h-[16em] mr-8  w-full h-full rounded-md bg-gradient-to-r from-fuchsia-600 via-violet-500 to-indigo-600 p-1'>
             <div className=' min-h-[16em] bg-white rounded-md p-4'>
               <div className='text-gray-500 font-semibold text-lg  mb-3'>Backend</div>
               <div className='text-gray-500 mb-2'>
@@ -88,7 +88,7 @@ export default function Featured() {
               </div>
             </div>
           </div>
-          <div className='min-h-[16em] min-w-[18em] mr-8  w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
+          <div className='min-h-[16em] min-w-[32em] mr-8  w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
             <div className='min-h-[16em] bg-white rounded-md p-4'>
               <div className='text-gray-500 font-semibold text-lg  mb-3'>UI/UX Design</div>
               <div className='text-gray-500 mb-2'>
@@ -96,7 +96,7 @@ export default function Featured() {
                 of custom CSS, Flexbox, and Grid. Additional strong skills with CSS frameworks such as: Tailwind,
                 Bootstrap, Material.UI, and Materialize.
               </div>
-              <div className='flex justify-between '>
+              <div className='flex justify-between mt-8'>
                 <Icon icon='simple-icons:tailwindcss' color='gray' width={40} height={40} />
                 <Icon icon='bi:bootstrap' color='gray' width={40} height={40} />
                 <Icon icon='mdi:material-ui' color='gray' width={40} height={40} />
@@ -106,7 +106,9 @@ export default function Featured() {
           </div>
         </div>
         <div className='flex'>
-          <div className='text-start font-semibold text-gray-500 mt-4'>Contact for all skills</div>
+          <div className='text-start ml-16 lg:ml-0 md:ml-0 xl:ml-0 font-semibold text-gray-500 mt-4'>
+            Contact for all skills
+          </div>
           <div className='mt-2'>
             <Icon icon='ic:outline-arrow-right' color='gray' width={40} height={40} />
           </div>
