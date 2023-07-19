da883a500e7a09e23e5cce68dcd23179d587d09c
emily <eap6787@gmail.com>
2023-07-19 12:51:29 -0600

    fix featured ui for tablet mode

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -26,7 +26,7 @@ export default function Bio() {
             className='flex justify-end'>
             <button
               type='button'
-              class='font-semibold mt-14 mr-12 py-2  text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2'>
+              class=' font-semibold mt-14 mr-12 py-2  text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2 '>
               Resume
             </button>
           </a>
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -5,9 +5,9 @@ export default function Featured() {
     <div>
       <div className='xl:ml-52 lg:ml-52 md:ml-16 xl:mr-52 lg:mr-52 md:mr-16 mt-10'>
         <div className='text-start ml-16 lg:ml-0 md:ml-0 xl:ml-0 text-2xl text-black font-bold'>Featured Skills</div>
-        <div className='hidden lg:flex md:flex  text-start mt-5'>
-          <div className=' min-w-[18em] min-h-[16em] mr-8   w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
-            <div className='min-h-[16em] bg-white rounded-md p-4'>
+        <div className='hidden md:hidden lg:flex md:flex  text-start mt-5'>
+          <div className=' min-w-[18em] min-h-[16em]  mr-8   w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
+            <div className='min-h-[16em] bg-white rounded-md p-4 '>
               <div className='text-gray-500 font-semibold text-lg  mb-3'>Frontend</div>
               <div className='text-gray-500 mb-8'>
                 Strong understanding of the React ecosystem, hooks, and state. High proficiency in Javascript, HTML,
@@ -23,7 +23,7 @@ export default function Featured() {
             </div>
           </div>
           <div className='min-w-[18em] min-h-[16em] mr-8  w-full h-full rounded-md bg-gradient-to-r from-fuchsia-600 via-violet-500 to-indigo-600 p-1'>
-            <div className=' min-h-[16em] bg-white rounded-md p-4'>
+            <div className=' min-h-[16em] bg-white rounded-md p-4 '>
               <div className='text-gray-500 font-semibold text-lg  mb-3'>Backend</div>
               <div className='text-gray-500 mb-2'>
                 Produced many applications using Node.js, MySQL, and Express.js. High understanding when it comes to
@@ -39,7 +39,7 @@ export default function Featured() {
             </div>
           </div>
           <div className='min-h-[16em] min-w-[18em] mr-8  w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
-            <div className='min-h-[16em] bg-white rounded-md p-4'>
+            <div className='min-h-[16em] bg-white rounded-md p-4 '>
               <div className='text-gray-500 font-semibold text-lg  mb-3'>UI/UX Design</div>
               <div className='text-gray-500 mb-2'>
                 Strong understanding of UI/UX design principles with an emphasis in usability and readability. Heavy use
@@ -55,7 +55,7 @@ export default function Featured() {
             </div>
           </div>
         </div>
-        <div className='flex-wrap ml-16 lg:hidden md:hidden xl:hidden  text-start mt-5'>
+        <div className='flex-wrap ml-16 lg:hidden xl:hidden  text-start mt-5'>
           <div className=' min-w-[32em] min-h-[16em] mb-5 w-full h-full rounded-md bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 p-1'>
             <div className='min-h-[16em] bg-white rounded-md p-4'>
               <div className='text-gray-500 font-semibold text-lg  mb-3'>Frontend</div>
@@ -64,7 +64,7 @@ export default function Featured() {
                 CSS, and DOM manipulation. Experience building applications using Jquery, and templating languages such
                 as Handlebars.js
               </div>
-              <div className='flex justify-between '>
+              <div className='flex justify-between md:mt-12'>
                 <Icon icon='akar-icons:react-fill' color='gray' width={40} height={40} />
                 <Icon icon='fluent:javascript-20-regular' color='gray' width={40} height={40} />
                 <Icon icon='dashicons:html' color='gray' width={40} height={40} />
@@ -80,7 +80,7 @@ export default function Featured() {
                 using and building RESTful APIs. Experience handling HTTP requests and third party APIs. Experience with
                 MongoDB, GraphQL, and websockets. Proficiency with Next.js
               </div>
-              <div className='flex justify-between'>
+              <div className='flex justify-between md:mt-10'>
                 <Icon icon='la:node' color='gray' width={40} height={40} />
                 <Icon icon='carbon:sql' color='gray' width={40} height={40} />
                 <Icon icon='teenyicons:nextjs-outline' color='gray' width={40} height={40} />
@@ -106,7 +106,7 @@ export default function Featured() {
           </div>
         </div>
         <div className='flex'>
-          <div className='text-start ml-16 lg:ml-0 md:ml-0 xl:ml-0 font-semibold text-gray-500 mt-4'>
+          <div className='text-start ml-16 lg:ml-0 md:ml-16 xl:ml-0 font-semibold text-gray-500 mt-4'>
             Contact for all skills
           </div>
           <div className='mt-2'>
