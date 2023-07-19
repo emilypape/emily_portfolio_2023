b794812a37e4bfcc37f9dcfe883793b8f78be31d
emily <eap6787@gmail.com>
2023-07-19 15:08:12 -0600

    add form submit link and email link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,7 +23,7 @@ export default function Contact() {
             <Icon icon='line-md:email-twotone' color='gray' width={40} height={40} />
             <div className='text-start ml-5'>
               <div className='font-bold'>Mail me at</div>
-              <a type='email'>
+              <a type='email' href='mailto:eap6787@gmail.com'>
                 <div className='font-extrabold text-transparent text-md bg-clip-text bg-gradient-to-r from-indigo-600 via-violet-500 to-fuchsia-600 '>
                   eap6787@gmail.com
                 </div>
@@ -36,10 +36,11 @@ export default function Contact() {
             <h2 class='py-3 mb-4 text-3xl tracking-tight font-bold text-end text-gray-900 dark:text-black'>
               Send me a message 🚀
             </h2>
-            <form action='#' class='space-y-8 min-w-[40em]'>
+            <form action='https://formsubmit.co/eap6787@gmail.com' method='POST' class='space-y-8 min-w-[40em]'>
               <div>
                 <input
                   type='email'
+                  name='email'
                   id='email'
                   class='shadow-sm bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 '
                   placeholder='email*'
@@ -49,26 +50,29 @@ export default function Contact() {
               <div>
                 <input
                   type='text'
+                  name='subject'
                   id='subject'
-                  class='block p-3 w-full text-sm text-gray-900 bg-white-50 rounded-lg border border-gray-400 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-white-700 dark:border-gray-300 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                  class='block p-3 w-full text-sm text-gray-900 bg-white-50 rounded-lg border border-gray-400 shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:border-gray-300 dark:placeholder-gray-400  dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                   placeholder='subject*'
                   required
                 />
               </div>
               <div class='sm:col-span-2'>
                 <textarea
+                  type='text'
+                  name='message'
                   id='message'
                   rows='6'
                   class='block p-2.5 w-full text-sm  bg-white-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 '
                   placeholder='Tell me a little more about the role...'></textarea>
               </div>
+              <button
+                type='submit'
+                class='font-semibold mt-10  py-2  text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300   rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-16'>
+                Send Message
+              </button>
             </form>
           </div>
-          <button
-            type='button'
-            class='font-semibold mt-10  py-2  text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300   rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-16'>
-            Send Message
-          </button>
         </section>
       </div>
     </div>
